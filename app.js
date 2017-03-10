f05e9591e589d1ef26dbf18db7313dec7dae10bc
Ben McCain <mccain.ben2@gmail.com>
2017-03-10 18:29:38 -0500

    in progress

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var nunjucks = require('nunjucks');
-var routes;
+var routes = require('./routes');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 
-app.engine('html', nunjucks.render);
-app.set('view engine', 'html');
-nunjucks.configure('views', {noCache: true});
+var env = nunjucks.configure('views', {noCache: true});
 // will need to change path from views...
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+
 
 app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '/somePath')));
+// app.use(express.static(path.join(__dirname, '/somePath')));
 //will need to change path...
 
-app.use('/', routes);
+// app.use('/', routes);
 
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+// var pg ???
+
+
+module.export = router;
